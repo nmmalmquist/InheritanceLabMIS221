3661b6f13127eef25439800f17696c50afe67895
nmmalmquist <nicholasmmalmquist@gmail.com>
2021-11-22 08:34:34 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mis221_lab_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
